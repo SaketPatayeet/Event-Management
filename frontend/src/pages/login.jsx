309ae91b953206772c693e1a47f30d22f44d6a36
SaketPatayeet <tsakep@gmail.com>
2026-06-17 22:11:37 +0530

    add docker setup

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-import './login.css';
+import './Login.css';
 
 // SVG Icons
 const UserIcon = () => (
